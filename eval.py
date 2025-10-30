604cb6ac4ad715de9406c27ce5cdeb05a6f60d49
Rajit Rajpal <rrajpal@mathsdgx1.maths.ed.ac.uk>
2025-10-30 14:50:19 +0000

    added a naive cubic muon

---
+++ eval.py
@@ -0,0 +1,45 @@
+import torch
+import torch.nn.functional as F
+
+############################################
+#               Evaluation                 #
+############################################
+
+def infer(model, loader, tta_level=0):
+
+    # Test-time augmentation strategy (for tta_level=2):
+    # 1. Flip/mirror the image left-to-right (50% of the time).
+    # 2. Translate the image by one pixel either up-and-left or down-and-right (50% of the time,
+    #    i.e. both happen 25% of the time).
+    #
+    # This creates 6 views per image (left/right times the two translations and no-translation),
+    # which we evaluate and then weight according to the given probabilities.
+
+    def infer_basic(inputs, net):
+        return net(inputs).clone()
+
+    def infer_mirror(inputs, net):
+        return 0.5 * net(inputs) + 0.5 * net(inputs.flip(-1))
+
+    def infer_mirror_translate(inputs, net):
+        logits = infer_mirror(inputs, net)
+        pad = 1
+        padded_inputs = F.pad(inputs, (pad,)*4, "reflect")
+        inputs_translate_list = [
+            padded_inputs[:, :, 0:32, 0:32],
+            padded_inputs[:, :, 2:34, 2:34],
+        ]
+        logits_translate_list = [infer_mirror(inputs_translate, net)
+                                 for inputs_translate in inputs_translate_list]
+        logits_translate = torch.stack(logits_translate_list).mean(0)
+        return 0.5 * logits + 0.5 * logits_translate
+
+    model.eval()
+    test_images = loader.normalize(loader.images)
+    infer_fn = [infer_basic, infer_mirror, infer_mirror_translate][tta_level]
+    with torch.no_grad():
+        return torch.cat([infer_fn(inputs, model) for inputs in test_images.split(2000)])
+
+def evaluate(model, loader, tta_level=0):
+    logits = infer(model, loader, tta_level)
+    return (logits.argmax(1) == loader.labels).float().mean().item()
---
+++ logs.py
@@ -0,0 +1,32 @@
+
+
+############################################
+#                 Logging                  #
+############################################
+
+logging_columns_list = ["run   ", "epoch", "train_acc", "val_acc", "tta_val_acc", "time_seconds"]
+
+def print_columns(columns_list, is_head=False, is_final_entry=False):
+    print_string = ""
+    for col in columns_list:
+        print_string += "|  %s  " % col
+    print_string += "|"
+    if is_head:
+        print("-"*len(print_string))
+    print(print_string)
+    if is_head or is_final_entry:
+        print("-"*len(print_string))
+
+def print_training_details(variables, is_final_entry):
+    formatted = []
+    for col in logging_columns_list:
+        var = variables.get(col.strip(), None)
+        if type(var) in (int, str):
+            res = str(var)
+        elif type(var) is float:
+            res = "{:0.4f}".format(var)
+        else:
+            assert var is None
+            res = ""
+        formatted.append(res.rjust(len(col)))
+    print_columns(formatted, is_final_entry=is_final_entry)
